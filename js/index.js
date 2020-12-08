a1112bdfee2deee5a805158fc98981275e9a9ea3
uihyeon9904 <62719364+uihyeon9904@users.noreply.github.com>
2020-12-08 17:48:47 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+
+	console.log($(".st96"))
+	var pathArray = $(".st96")
+
+	$.each(pathArray, function (i, d) {
+
+
+		var pathLength = d.getTotalLength()
+		console.log(pathLength)
+
+		$(d).css("stroke-dasharray" , pathLength + " " + pathLength)
+		$(d).css("stroke-dashoffset" , pathLength)
+	})
+})
+
+$(document).ready(function () {
+
+	console.log($(".st97"))
+	var pathArray = $(".st97")
+
+	$.each(pathArray, function (i, d) {
+
+
+		var pathLength = d.getTotalLength()
+		console.log(pathLength)
+
+		$(d).css("stroke-dasharray" , pathLength + " " + pathLength)
+		$(d).css("stroke-dashoffset" , pathLength)
+	})
+})
+
+$(document).ready(function () {
+
+	console.log($(".st98"))
+	var pathArray = $(".st98")
+
+	$.each(pathArray, function (i, d) {
+
+
+		var pathLength = d.getTotalLength()
+		console.log(pathLength)
+
+		$(d).css("stroke-dasharray" , pathLength + " " + pathLength)
+		$(d).css("stroke-dashoffset" , pathLength)
+	})
+})
+
+$(document).ready(function () {
+
+	console.log($(".st99"))
+	var pathArray = $(".st99")
+
+	$.each(pathArray, function (i, d) {
+
+
+		var pathLength = d.getTotalLength()
+		console.log(pathLength)
+
+		$(d).css("stroke-dasharray" , pathLength + " " + pathLength)
+		$(d).css("stroke-dashoffset" , pathLength)
+	})
+})
